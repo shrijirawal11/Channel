30f316d90908400e56b27ba6200e25daf369847d
shrijirawal11 <shrjirawal@gmail.com>
2023-11-09 09:08:29 +0530

    change in admin

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import writer,post
 # Register your models here.
+@admin.register(writer)
+class writerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'user')
+    search_fields = ('name', 'email') 
+    
